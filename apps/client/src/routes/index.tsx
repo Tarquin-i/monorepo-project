3f42acb56d888ddb6bfc63c2cfb3725f18719888
Tarquin <3442322864@qq.com>
2026-03-27 19:27:23 +0800

    前端依赖

--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Camera } from 'lucide-react';
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
       <>
-      <div>
+      <Camera/>
+      <div className='text-red-300'>
         hello world!!
       </div>
       </>
