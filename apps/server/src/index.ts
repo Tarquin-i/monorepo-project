7a8318024bd8ed12e1690d6cbd9579436348d29c
Tarquin <3442322864@qq.com>
2026-03-27 19:42:37 +0800

    后端安装 zod & openapi

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,64 @@
-import { Hono } from 'hono'
+import { OpenAPIHono, createRoute } from '@hono/zod-openapi';
+import { z } from 'zod';
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const ParamsSchema = z.object({
+  id: z
+    .string()
+    .min(3)
+    .openapi({
+      param: {
+        name: 'id',
+        in: 'path',
+      },
+      example: '1212121',
+    }),
+});
 
-export default app
+const UserSchema = z
+  .object({
+    id: z.string().openapi({
+      example: '123',
+    }),
+    name: z.string().openapi({
+      example: 'John Doe',
+    }),
+    age: z.number().openapi({
+      example: 42,
+    }),
+  })
+  .openapi('User');
+app.openapi(
+  createRoute({
+    method: 'get',
+    path: '/users/{id}',
+    request: {
+      params: ParamsSchema,
+    },
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: UserSchema,
+          },
+        },
+        description: 'Retrieve the user',
+      },
+    },
+  }),
+  (c) => {
+    const { id } = c.req.valid('param');
+    return c.json({
+      id: '1',
+      age: 20,
+      name: 'Ultra-man',
+    });
+  },
+);
+
+// app.get('/', (c) => {
+//   return c.text('Hello Hono!')
+// })
+
+export default app;
